e883d55c4515f143bc7846de9e6153cbe29962db
Ray Klump Lewis University <klumpra@lewisu.edu>
2021-06-27 14:13:14 -0500

    Added another line.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 print("Hello world!")
 print("Check me out!")
-print("Putting this line back in.")
+print("Putting this line back in. I confirmed that I want this.")
+print("git can be confusing, but it is great when you get the hang of it.")
